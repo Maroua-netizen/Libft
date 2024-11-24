c28ae2649f4fec0b268bebe0e15bbbe9138e0c4a
Maroua Mounsif <mmounsif@c2r9p6.1337.ma>
2024-11-24 11:00:18 +0100

    Libft

---
+++ Libft/ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmounsif <mmounsif@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 18:04:24 by mmounsif          #+#    #+#             */
+/*   Updated: 2024/11/18 00:11:21 by mmounsif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+// 	printf("%lu\n", strlen("test"));
+// }
